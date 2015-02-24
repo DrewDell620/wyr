840db996b42025665703631a662560843606de20
DrewDell620 <drewdell620@gmail.com>
2015-02-24 06:10:56 -0600

    htdocs
    These are the files needed for when a user comes to the home login page,
    because of a redirect.

---
+++ htdocs/register.php
@@ -0,0 +1,13 @@
+<?php
+
+// load the login class
+require_once('includes.php');
+
+// create a login object. when this object is created, it will do all login/logout stuff automatically
+// so this single line handles the entire login process.
+$login = new Login();
+
+// shows the about page
+include("public/register.php");
+
+?>
